0fb224e3d6b77c565b9d28a559ca510af2e0ba3f
Tomaz <tomazcraft@gmail.com>
2025-06-22 08:28:26 -0300

    Foi criado um resource para criar autenticação de usuarios e um metodo para fazer registros

--- back-end/finance/src/main/java/com/tomaz/finance/resources/AuthResource.java
+++ back-end/finance/src/main/java/com/tomaz/finance/resources/AuthResource.java
@@ -1,5 +1,30 @@
 package com.tomaz.finance.resources;
 
-public class AuthResource {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tomaz.finance.dto.UserCreateDTO;
+import com.tomaz.finance.entities.User;
+import com.tomaz.finance.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@PostMapping("/register")
+	public ResponseEntity<User> register(@Valid @RequestBody UserCreateDTO dto){
+		User obj = service.create(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+	}
+	
 }
