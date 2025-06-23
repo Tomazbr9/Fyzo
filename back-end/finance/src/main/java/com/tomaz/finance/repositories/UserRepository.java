6c639ee334e6e9a7b61581774d45f0d0a37110e1
Tomaz <tomazcraft@gmail.com>
2025-06-23 10:32:09 -0300

    Foi criado um metodo no repositorio user para buscar no banco de dados informações do usuario

--- back-end/finance/src/main/java/com/tomaz/finance/repositories/UserRepository.java
+++ back-end/finance/src/main/java/com/tomaz/finance/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.tomaz.finance.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository; 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tomaz.finance.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+	Optional<User> findByUsername(String username);
 }
