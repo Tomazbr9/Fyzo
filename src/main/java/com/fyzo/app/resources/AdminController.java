31c26f20fa126ae04577df51444bd25a21e58ac6
Tomaz <tomazcraft@gmail.com>
2025-08-03 11:43:56 -0300

    feat(service): adicionar @Transactional nos métodos update e delete do UserService

--- src/main/java/com/fyzo/app/resources/AdminController.java
+++ src/main/java/com/fyzo/app/resources/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +32,7 @@ import jakarta.validation.Valid;
 	)
 @RestController
 @RequestMapping("/admin")
-public class AdminResource {
+public class AdminController {
 	
 	@Autowired
 	private AdminService service;
@@ -143,7 +144,7 @@ public class AdminResource {
 		    }
 		)
 		@PreAuthorize("hasRole('ADMIN')")
-		@PatchMapping("/users/{id}")
+		@DeleteMapping("/users/{id}")
 		public ResponseEntity<Void> delete(@PathVariable Long id) {
 		    service.delete(id);
 		    return ResponseEntity.noContent().build();
